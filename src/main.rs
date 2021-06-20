2615fc48487ddb0fa505c01eba35c6988e3684fa
IsaccBarker <plutoisaplanet4324@gmail.com>
2021-06-20 16:50:09 -0600

    final commit before ending?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(in_band_lifetimes, proc_macro_hygiene, decl_macro)]
+
 mod interaction;
 
 #[macro_use]
@@ -5,17 +7,42 @@ extern crate rocket;
 use log::info;
 use interaction::Person;
 use interaction::{get_record, get_record_str, save_record};
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket_contrib::serve::StaticFiles;
+
+pub struct CORS;
+
+impl Fairing for CORS {
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+}
+
 
 #[get("/submit_data/<name>/<number>/<likes>")]
 fn submit_data(name: String, number: String, likes: String) -> String {
     let mut record = get_record();
+    let decoded_name = urlencoding::decode(name.as_str()).unwrap();
+    let decoded_number = urlencoding::decode(number.as_str()).unwrap();
     let decoded_likes = urlencoding::decode(likes.as_str()).unwrap();
 
-    info!("Data submitted!\n\tname: {}\n\t\n\tnumber: {}\n\tlikes: {}\n\tdecoded likes: {}", name, number, likes, &decoded_likes);
+    info!("Data submitted!\n\tname: {}\n\t\n\tnumber: {}\n\tlikes: {}", &decoded_name, &decoded_number, &decoded_likes);
 
     record.people.push(Person {
-        name,
-        number,
+        name: decoded_name,
+        number: decoded_number,
         likes: serde_json::from_str::<Vec<String>>(
             &decoded_likes
         ).unwrap(),
@@ -37,7 +64,8 @@ fn get_data() -> String {
     data
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![get_data, submit_data])
+fn main() {
+    rocket::ignite().attach(CORS)
+        .mount("/", routes![get_data, submit_data])
+        .mount("/", StaticFiles::from("docs")).launch();
 }
